9db047503f87130699d36e3182355e1ca8940e11
Daniel Ekerhovd <danielekerh@gmail.com>
2024-06-01 12:05:02 +0200

    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     content: ["./**/*.html"],
     theme: {
       extend: {
+        colors: {
+          background: "#F1E5D1",
+          lightFocus: "#987070",
+          darkFocus: "#DBB5B5",
+          mediumFocus: "#C39898"
+        },
         fontFamily: {
           sans: ["Poppins", "sans-serif"],
         }
