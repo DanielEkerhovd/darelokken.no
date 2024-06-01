008319ab0fd9a4ec2d0b9943aa11a256eaaa9a6a
Daniel Ekerhovd <danielekerh@gmail.com>
2024-06-01 13:04:32 +0200

    Worked on the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
       extend: {
         colors: {
           lightFocus: "#F1E5D1",
-          darkFocus: "#987070",
+          background: "#987070",
           mediumFocus: "#DBB5B5",
-          background: "#C39898"
+          darkFocus: "#C39898",
         },
         fontFamily: {
-          sans: ["Poppins", "sans-serif"],
-        }
+          sans: ["'Poppins'", "sans-serif"],
+        },
       },
     },
     plugins: [],
