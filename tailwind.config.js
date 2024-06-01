29d45dba2fd97a66bf2fa12b598146de8254212b
Daniel Ekerhovd <danielekerh@gmail.com>
2024-06-01 12:09:05 +0200

    Corrected color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-          background: "#F1E5D1",
-          lightFocus: "#987070",
-          darkFocus: "#DBB5B5",
-          mediumFocus: "#C39898"
+          lightFocus: "#F1E5D1",
+          darkFocus: "#987070",
+          mediumFocus: "#DBB5B5",
+          background: "#C39898"
         },
         fontFamily: {
           sans: ["Poppins", "sans-serif"],
