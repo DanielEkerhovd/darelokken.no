f7f52de6f62ef3bc21a375512331339f19ea7245
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-06-03 16:45:27 +0200

    Added timer function

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ priserButton.addEventListener('click', () => {
     sePriser.scrollIntoView({behavior: "smooth"});
 });
 
+// Today's date
+
+const openingHours = {
+
+    weekdays: {
+        open: '08:00',
+        close: '17:00'
+    },
+    saturday: {
+        open: '09:00',
+        close: '14:00'
+    },
+    sunday: {
+        open: 'closed',
+        close: 'closed'
+    }
+};
+
+const todayContainer = document.getElementById('hoursToday');
+
+const today = new Date();
+const day = today.getDay();
+
+todayContainer.innerHTML = ""
+
+if (day === 0) {
+    todayContainer.innerHTML = `${openingHours.sunday.close}`;
+} else if (day === 6) {
+    todayContainer.innerHTML = `${openingHours.saturday.open} - ${openingHours.saturday.close}`;
+} else {
+    todayContainer.innerHTML = `${openingHours.weekdays.open} - ${openingHours.weekdays.close}`;
+}
