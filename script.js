b217d03994a38c959471adeab450c4a8cc30ee0d
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-06-03 17:53:00 +0200

    added alt to image

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ if (day === 0) {
 const heroContent = document.getElementById('heroContent');
 const heroImage = document.createElement('img');
 heroImage.classList.add('h-full');
+heroImage.alt = "Hero image";
 
 const heroImages = ['src/media/model1.png', 'src/media/model2.png', 'src/media/model3.png'];
 heroImage.src = heroImages[Math.floor(Math.random() * heroImages.length)];
